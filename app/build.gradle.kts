e522c91815355008019d30e0b8a5851b1214d182
Artur Fayzullin <fayzullin.ar@mail.ru>
2025-05-11 12:09:15 +0300

    add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("org.sonarqube") version "6.0.1.5171"
+    checkstyle
 }
 
 group = "hexlet.code"
@@ -17,12 +18,4 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
-}
-
-sonar {
-    properties {
-        property("sonar.projectKey", "Bascy6_java-project-72")
-        property("sonar.organization", "bascy6")
-        property("sonar.host.url", "https://sonarcloud.io")
-    }
 }
