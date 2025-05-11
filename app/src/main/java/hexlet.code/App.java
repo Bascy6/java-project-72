c8d56cbcbb7c0b06cb4c48b25078bec9fb3d886a
Artur Fayzullin <fayzullin.ar@mail.ru>
2025-05-11 14:28:31 +0300

    add

--- app/src/main/java/hexlet.code/App.java
+++ app/src/main/java/hexlet.code/App.java
@@ -18,7 +18,7 @@ public class App {
     }
 
     private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "7070");
+        String port = System.getenv().getOrDefault("PORT", "8080");
         return Integer.parseInt(port);
     }
 }
